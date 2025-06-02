e3787943cbd5c89586bdbf5a368f028dcca85065
juhyg-hash <gusrb1474@gmail.com>
2025-06-02 09:59:52 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        final int DAYS_IN_WEEK = 7;
 
+
+
+//        a = 11;
+//        프로그래밍 언어에서 '='은 오른쪽에 값을 왼쪽으로 할당한다 (assign) final vairable constant variable
+//        주석 처리 단축키 컨트롤+ /
     }
 }
