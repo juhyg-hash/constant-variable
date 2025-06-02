933f3595942154a5c1f4cf88c34e3bdc4448f37f
juhyg-hash <gusrb1474@gmail.com>
2025-06-02 14:14:50 +0900

    seventh commit

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        int num = -1;
 
-        int num = 5;
 
-//        if(조건문){실해문;}
-        if(num > 0){
-            System.out.println("number는 0보다 큽니다");
+
+        if (num > 0){
+            System.out.println("num은 양수입니다");
+        } else if (num < 0) {
+            System.out.println("num은 음수입니다");
+        } else {
+            System.out.println("num은 0입니다");
         }
-
-        if(num > 8){
-            System.out.println("number는 0보다 큽니다");
-        } else{
-            System.out.println("number는 8 보다 작다");
-        }
-
-
-        if(num >= 5){
-            System.out.println("num은 0보다 큽니다");
-        }
-
     }
 }
