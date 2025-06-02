92c77da4fe35434b8f6076be105ee7a04ddc1eac
juhyg-hash <gusrb1474@gmail.com>
2025-06-02 10:34:37 +0900

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        final int DAYS_IN_WEEK = 7;
+        int a = 10, b = 3;
 
+        int sum = a +b;
+        System.out.println("sum = " + sum);
 
+        int difference =  a - b;
+        System.out.println("difference = " + difference);
 
-//        a = 11;
-//        프로그래밍 언어에서 '='은 오른쪽에 값을 왼쪽으로 할당한다 (assign) final vairable constant variable
-//        주석 처리 단축키 컨트롤+ /
+        int product = a * b;
+        System.out.println("product = " + product);
+
+        int quotient = a / b;
+        System.out.println("quotient = " + quotient);
+
+        int remainder = a % b;
+        System.out.println("remainder = " + remainder);
+
     }
 }
