449ca3d169c5e7398f10c691228df89df5748e4b
juhyg-hash <gusrb1474@gmail.com>
2025-06-02 13:27:41 +0900

    sixth commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
+        int num = 5;
+
+//        if(조건문){실해문;}
+        if(num > 0){
+            System.out.println("number는 0보다 큽니다");
+        }
+
+        if(num > 8){
+            System.out.println("number는 0보다 큽니다");
+        } else{
+            System.out.println("number는 8 보다 작다");
+        }
+
+
+        if(num >= 5){
+            System.out.println("num은 0보다 큽니다");
+        }
+
     }
 }
